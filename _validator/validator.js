fcfd2b91eed1d12b852a4b61320432588f441efd
hamlet-test <45740223+hamlet-test@users.noreply.github.com>
2018-12-16 15:44:04 +0400

    Update validator.js

--- _validator/validator.js
+++ _validator/validator.js
@@ -1,4 +1,7 @@
 "use strict";
-const assert = require('assert');
-const colors = require('colors');
+var fs = require('fs');
+var colors = require('colors');
+var request = require('request');
+var PythonShell = require('python-shell');
+var assert = require('assert');
 assert(0);
